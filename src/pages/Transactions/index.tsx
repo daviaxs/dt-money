7e93489df61c31045de3dbe7cd8bbe9105f68a3a
Davi Alves <cadwvk@gmail.com>
2023-09-07 16:15:58 -0300

    Created Summary component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from '../../shared/components/Header'
+import { Summary } from '../../shared/components/Summary'
 
 export function Transactions() {
   return (
     <div>
       <Header />
+      <Summary />
     </div>
   )
 }
