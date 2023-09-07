e4ce4e8d973c3ad6b9c8545388931031d8af8772
Davi Alves <cadwvk@gmail.com>
2023-09-07 15:17:55 -0300

    Created GlobalStyle and add styled type

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
-  return <>Hello world</>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello world</h1>
+    </ThemeProvider>
+  )
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    &:focus {
+      outline: 0;
+      box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
+    }
+  }
+`
