5cf3c2ba312ea8408d5930a8b88b96e70461acf3
Davi Alves <cadwvk@gmail.com>
2023-09-07 16:31:46 -0300

    Created table in Transactions page

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -6,6 +6,25 @@ export function Transactions() {
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <table>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>R$ 12.000,00</td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>- R$ 59,00</td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </table>
+      </TransactionsContainer>
     </div>
   )
 }
